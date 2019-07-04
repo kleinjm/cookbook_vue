7d87013f7cd1b2f52154aadd84b81103d4007c79
James Klein <kleinjm007@gmail.com>
2019-07-04 11:14:59 -0400

    rename env vars

--- config/env.js
+++ config/env.js
@@ -3,7 +3,7 @@
  ** See https://nuxtjs.org/api/configuration-env
  */
 export default {
-  baseUrl: process.env.BASE_URL || 'http://localhost:3001/api',
-  host: process.env.HOST || 'localhost',
-  port: process.env.PORT || 3001,
+  baseUrl: process.env.BACKEND_BASE_URL || 'http://localhost:3001/api',
+  host: process.env.BACKEND_HOST || 'localhost',
+  port: process.env.BACKEND_PORT || 3001,
 }
