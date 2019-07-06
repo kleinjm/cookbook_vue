69f2121aec1e01b82535b409dcf11417ff9ea710
James Klein <kleinjm007@gmail.com>
2019-07-05 22:13:35 -0400

    styling

--- config/vuetify.js
+++ config/vuetify.js
@@ -1,17 +1,15 @@
-import colors from 'vuetify/es5/util/colors'
-
 /*
  ** vuetify module configuration
  ** https://github.com/nuxt-community/vuetify-module
  */
 export default {
   theme: {
-    primary: colors.blue.darken2,
-    accent: colors.grey.darken3,
-    secondary: colors.amber.darken3,
-    info: colors.teal.lighten1,
-    warning: colors.amber.base,
-    error: colors.deepOrange.accent4,
-    success: colors.green.accent3,
+    primary: '#009688',
+    secondary: '#607d8b',
+    accent: '#03a9f4',
+    error: '#ff5722',
+    warning: '#795548',
+    info: '#0540ec',
+    success: '#4caf50',
   },
 }
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,34 +1,21 @@
 <template>
   <v-app light>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
+    <v-navigation-drawer v-model="drawer" fixed app right>
       <v-alert type="error" :value="error">{{ error }}</v-alert>
       <v-list>
-        <v-list-tile
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
+        <v-list-tile>
           <v-list-tile-action>
-            <v-icon>{{ item.icon }}</v-icon>
+            <v-icon>app</v-icon>
           </v-list-tile-action>
           <v-list-tile-content>
-            <v-list-tile-title v-text="item.title" />
+            <v-list-tile-title>
+              {{ $auth.$state.user.first_name }}
+              {{ $auth.$state.user.last_name }}
+            </v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
         <v-list-tile v-if="$auth.$state.loggedIn" @click="logout">
           <v-list-tile-content>
-            <v-list-tile-title
-              >Logged in as
-              {{ $auth.$state.user.first_name }}</v-list-tile-title
-            >
             <v-list-tile-title>Sign out</v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
@@ -46,22 +33,38 @@
         </template>
       </v-list>
     </v-navigation-drawer>
-    <v-toolbar :clipped-left="clipped" fixed app>
-      <v-toolbar-side-icon @click="drawer = !drawer" />
-      <v-btn icon @click.stop="miniVariant = !miniVariant">
-        <v-icon>{{ `chevron_${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="clipped = !clipped">
-        <v-icon>web</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="fixed = !fixed">
-        <v-icon>remove</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
+    <v-toolbar fixed app color="primary">
+      <router-link to="/" class="white--text">
+        <v-toolbar-title>
+          Cookbook
+        </v-toolbar-title>
+      </router-link>
       <v-spacer />
-      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
-        <v-icon>menu</v-icon>
-      </v-btn>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn flat class="white--text">
+          <v-badge color="info" left>
+            <template v-slot:badge>
+              <span>4</span>
+            </template>
+            Up Next
+          </v-badge>
+        </v-btn>
+        <template v-if="$auth.$state.loggedIn">
+          <v-btn flat @click="drawer = !drawer">
+            <v-icon class="white--text">
+              account_circle
+            </v-icon>
+          </v-btn>
+        </template>
+        <template v-else>
+          <v-btn to="/login" router flat class="white--text">
+            Sign In
+          </v-btn>
+          <v-btn to="/sign-up" router flat class="white--text">
+            Sign Up
+          </v-btn>
+        </template>
+      </v-toolbar-items>
     </v-toolbar>
     <v-content>
       <v-container>
@@ -76,19 +79,7 @@
         <nuxt />
       </v-container>
     </v-content>
-    <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
-      <v-list>
-        <v-list-tile @click.native="right = !right">
-          <v-list-tile-action>
-            <v-icon light>
-              compare_arrows
-            </v-icon>
-          </v-list-tile-action>
-          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer :fixed="fixed" app>
+    <v-footer app>
       <span>&copy; 2019</span>
     </v-footer>
   </v-app>
@@ -100,25 +91,8 @@ import { mapActions, mapGetters } from 'vuex'
 export default {
   data() {
     return {
-      clipped: false,
       drawer: false,
-      fixed: false,
-      items: [
-        {
-          icon: 'apps',
-          title: 'Welcome',
-          to: '/',
-        },
-        {
-          icon: 'bubble_chart',
-          title: 'Inspire',
-          to: '/inspire',
-        },
-      ],
-      miniVariant: false,
       right: true,
-      rightDrawer: false,
-      title: 'Cookbook',
       error: null,
       displayGlobalAlert: false,
     }
@@ -147,6 +121,7 @@ export default {
   methods: {
     ...mapActions(['clearGlobalAlert']),
     logout() {
+      this.drawer = false
       this.$auth
         .logout()
         .then(() => {
